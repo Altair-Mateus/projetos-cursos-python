8442fd543fe49c0f55828426739ed5e180cd6e26
Altair-Mateus <taicoalencastro@gmail.com>
2023-01-26 19:10:05 -0300

    first commit

---
+++ jogos/adivinhacao.py
@@ -0,0 +1,31 @@
+print("**********************************")
+print("Bem vindo ao jogo de adivinhação!")
+print("**********************************")
+
+numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
+
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {} ".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu número: ")
+
+    print("Você digitou: ", chute_str)
+
+    chute = int(chute_str)
+
+    acertou =  numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if(maior):
+            print("Você errou! Seu chute foi maior que o número secreto")
+        elif(menor):
+            print("Você errou! Seu chute foi menor que o número secreto")
+
+print("Fim de Jogo")
+
+
