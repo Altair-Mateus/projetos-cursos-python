838dc7aafc1dcb264164ce5c9b2d13408bc4c0fe
Altair-Mateus <taicoalencastro@gmail.com>
2023-02-09 21:11:54 -0300

    Incluído níveis de dificuldade e pontuações

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -3,11 +3,25 @@ import random
 
 print("**********************************")
 print("Bem vindo ao jogo de adivinhação!")
-print("**********************************")
+print("**********************************\n")
 
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
+total_de_tentativas = 0
 rodada = 1
+pontos = 1000
+
+print("(1) FÁCIL | (2) MÉDIO | (3) DIFICIL")
+nivel = int(input("Escolha o nível do Jogo: "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+elif (nivel == 3):
+    total_de_tentativas = 5
+else:
+    print("NÍVEL INVÁLIDO!")
+
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {} ".format(rodada, total_de_tentativas))
@@ -18,7 +32,7 @@ for rodada in range(1, total_de_tentativas + 1):
     chute = int(chute_str)
 
     if (chute < 1 or chute > 100):
-        print("Digite apenas numeros entre 1 e 100!")
+        print("Digite apenas numeros entre 1 e 100!\n")
         continue
 
     acertou =  numero_secreto == chute
@@ -27,14 +41,21 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if (acertou):
         print("Você acertou!")
+
+        print("Pontuação: {}".format(pontos))
+
         break
     else:
         if(maior):
-            print("Você errou! Seu chute foi maior que o número secreto")
+            print("Você errou! Seu chute foi maior que o número secreto\n")
         elif(menor):
-            print("Você errou! Seu chute foi menor que o número secreto")
+            print("Você errou! Seu chute foi menor que o número secreto\n")
+
+        pontos_perdidos = abs(chute - numero_secreto)
+        pontos = pontos - pontos_perdidos
 
 print("Numero secreto: ", numero_secreto)
+print("Pontuação: {}".format(pontos))
 print("Fim de Jogo")
 
 
